986769eb4152a17735515b9bc2e8f431c19f33a7
zorpid <robin96.malmo@gmail.com>
2025-02-21 11:43:36 +0100

    inital Commit
    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RecipeWebbApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RecipeWebbApplication
+ 
