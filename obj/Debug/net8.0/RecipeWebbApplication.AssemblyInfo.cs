baa0f5be27846389896ad993bafd652a9d21d0d8
Johan Cederblad <johanzcederblad@hotmail.com>
2025-03-07 14:10:01 +0100

    PhotoUrl
    IMG-default-photo

--- obj/Debug/net8.0/RecipeWebbApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/RecipeWebbApplication.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeWebbApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a6dd1e7e571c36bcc59e2ba1aab0bab1984ea6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2739a7828851732923aadfa827bf34699cc378aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeWebbApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeWebbApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
